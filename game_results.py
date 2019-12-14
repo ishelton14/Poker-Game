0b4e2f028883026c34775bf465f72663e234c84e
ishelton14 <ianshelt@gmail.com>
2019-12-14 12:40:23 -0500

    only evaluate tie breaker if both player scores are not None

--- game_results.py
+++ game_results.py
@@ -23,19 +23,23 @@ for row in rows:
     player_a_score = evaluate_hand(player_a_hand)
     player_b_score = evaluate_hand(player_b_hand)
 
-    player_a_tie_breaker, player_a_score = get_tie_breaker(card_order, player_a_score)
-    player_b_tie_breaker, player_b_score = get_tie_breaker(card_order, player_b_score)
-
+    # If neither player gets at least a pair, get highest card
     if player_a_score is None and player_b_score is None:
         winner = highest_card(player_a_hand, player_b_hand)
 
+    # If player a or player b gets at least one pair, but the other player does not,
+    # the player with at least one pair wins
     elif player_a_score is None and player_b_score is not None:
         winner = 1
-
     elif player_a_score is not None and player_b_score is None:
         winner = 0
 
+    # If both players get at least one pair, check scores
     else:
+
+        # Separate tie breaker and score value
+        player_a_tie_breaker, player_a_score = get_tie_breaker(card_order, player_a_score)
+        player_b_tie_breaker, player_b_score = get_tie_breaker(card_order, player_b_score)
 
         if player_a_score < player_b_score:
             winner = 0
@@ -45,6 +49,7 @@ for row in rows:
 
         else:
 
+            # If both players have same the score and both have tie breaker values, then check tie breaker values
             if player_a_tie_breaker is not None and player_b_tie_breaker is not None:
 
                 if player_a_tie_breaker > player_b_tie_breaker:
